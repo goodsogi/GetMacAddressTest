de2a6369e143ff68187d3a982a46a749ab51a1b4
Jeonggyu <goodsogi@gmail.com>
2016-09-23 14:20:37 +0900

    first commit

---
+++ app/src/main/java/com/commax/getmacaddresstest/MainActivity.java
@@ -0,0 +1,88 @@
+package com.commax.getmacaddresstest;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showMacAddress();
+
+
+    }
+
+    private void showMacAddress() {
+        TextView macAddress = (TextView) findViewById(R.id.macAddress);
+        macAddress.setText(getMacAddress());
+    }
+
+    public static String loadFileAsString(String filePath) throws java.io.IOException{
+        StringBuffer data = new StringBuffer(1000);
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        char[] buf = new char[1024];
+        int numRead=0;
+        while((numRead=reader.read(buf)) != -1){
+            String readData = String.valueOf(buf, 0, numRead);
+            data.append(readData);
+        }
+        reader.close();
+        return data.toString();
+    }
+
+    public String getMacAddress(){
+        try {
+            return loadFileAsString("/sys/class/net/eth0/address")
+                    .toUpperCase().substring(0, 17);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+//    private String getMacAddress() {
+//        WifiManager manager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+//        WifiInfo info = manager.getConnectionInfo();
+//       return info.getMacAddress();
+//    }
+
+//    public static String getMacAddress() {
+//        try {
+//            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+//            for (NetworkInterface nif : all) {
+//                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+//
+//                byte[] macBytes = nif.getHardwareAddress();
+//                if (macBytes == null) {
+//                    return "";
+//                }
+//
+//                StringBuilder res1 = new StringBuilder();
+//                for (byte b : macBytes) {
+//                    res1.append(String.format("%02X:",b));
+//                }
+//
+//                if (res1.length() > 0) {
+//                    res1.deleteCharAt(res1.length() - 1);
+//                }
+//                return res1.toString();
+//            }
+//        } catch (Exception ex) {
+//        }
+//        return "02:00:00:00:00:00";
+//    }
+}
